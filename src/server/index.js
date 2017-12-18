ccdec450fc3a2d9407c4ecce47be56ae284125b6
Shaffiullah G <shaffiullah.g@causeway.com>
2017-12-18 22:44:17 +0000

    add express Midlewares

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const app = express();
 
 // Set PORT and HOST for the Web server
 const APP_PORT = (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || '3000';
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
+
+/*
+  req.body Contains key-value pairs of data submitted in the request body.
+  By default, it is undefined, and is populated when you use body-parsing middleware
+  such as body-parser
+*/
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+/*
+  Gzip compressing can greatly decrease the size of the response body and hence increase the speed of a web app
+ */
+app.use(compression());
 
 app.set('port', APP_PORT);
 app.set('host', APP_HOST);
@@ -15,15 +30,13 @@ app.set('host', APP_HOST);
   Once set, the value of app.locals properties persist throughout the life of the application,
   in contrast with res.locals properties that are valid only for the lifetime of the request.
 */
-
 app.locals.title = process.env.APP_NAME || 'Bucephalus';
 app.locals.version = process.env.APP_VERSION || '1.0.0';
 
-// Everything in the public folder is served as static content
+/*
+  To serve static files such as images, CSS files, and JavaScript files
+*/
 app.use(express.static(path.join(__dirname, '/../public')));
-
-// routes
-app.get('/', (req, res) => res.send('Hello Wolld!'))
 
 /*
   Binds and listens for connections on the specified host and port.
